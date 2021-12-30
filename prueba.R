38d5562fce0325b673e9fdd1fbc3b9b8ed822536
Ricardo <rik990@gmail.com>
2021-12-30 16:29:46 -0300

    second commit

---
+++ prueba.R
@@ -0,0 +1,8 @@
+library(here)
+
+# feather::write_feather(iris,"data/iris1.feather")
+# feather::write_feather(cars,"data/sub-data/cars1.feather")
+
+feather::read_feather(here("data","iris1.feather"))
+
+feather::read_feather(here("data","sub-data","cars1.feather"))
